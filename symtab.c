0caf3d55b51dca55f8c0ab83628d0c8dc92bd144
JerryChen <jerrychen210054@gmail.com>
2016-10-22 21:58:03 +0800

    [Jerry] 10/22 Bad commit, undo.

--- symtab.c
+++ symtab.c
@@ -46,7 +46,6 @@ void semanticCheck(struct nodeType *node) {
 
         // Declaration part, add to symbol table.
         case NODE_VAR_DECL: {
-            // TODO loop inside the rsibling of NODE_VAR_DECL.
 
             struct nodeType *typeNode = nthChild(1, node);
             enum StdType valueType;
@@ -66,34 +65,53 @@ void semanticCheck(struct nodeType *node) {
               }
 
               struct nodeType *idList = nthChild(1, typeNode);
+              struct nodeType *arrType = typeNode->child;
               struct nodeType *idNode = idList->child;
+              
+              /* For ArrayType Declaration. */
               if(valueType == TypeArray){
-                // TODO array . extract the start end from node.
+
+                while (arrType->nodeType==NODE_TYPE_ARRAY){
+                  // Traverse to find the declared type of arrays.
+                  arrType = arrType->child;
+                }
+                
                 while(idList->nodeType != NODE_LIST){
+                  // Find the TokenList.
                   idList = idList->rsibling;
                 }
                 idNode = idList->child;
-
                 
+                // Add all token under the idList.
+                do {
+                  if(!findSymbol(idNode->string))
+                    addVariable(idNode->string, arrType->valueType, typeNode);
+                  idNode = idNode->rsibling;
+                  // FIXME Should happen infinite loop here when only one child. 
+                  // BUT it didn't happen ?!
+                } while(idNode != idList->child);
+
               }
-
-              do {
-                addVariable(idNode->string, valueType, typeNode);
-                idNode = idNode->rsibling;
-              } while(idNode != idList->child);
-
+              /* For Int and Real declaration. */
+              else{
+                do {
+                  addVariable(idNode->string, valueType, typeNode);
+                  idNode = idNode->rsibling;
+                } while(idNode != idList->child);
+              }
               typeNode = typeNode -> rsibling;
 
             }while(typeNode != node->child);
             return;
         }
+        /** End of Declaration. **/
+        
+        /* General cases in the Compoud statement. 
+           Focus on array stuff.
+        */
 
         /* This case is simplified, actually you should check
            the symbol is a variable or a function with no parameter */
-        case NODE_ARR_REF:
-          printf("ARR_REF: lefttype:%d, righttype:%d\n",
-                  node->child->nodeType, node->child->rsibling->nodeType);
-          return;
         case NODE_VAR_OR_PROC: 
         case NODE_SYM_REF: {
             struct SymTableEntry *entry = findSymbol(node->string);
@@ -108,8 +126,64 @@ void semanticCheck(struct nodeType *node) {
             return;
         }
         
+        case NODE_ARR_REF: {
+            struct nodeType *child1, *child2;
+            struct nodeType *arrnode=NULL;
+            struct nodeType *arrtype = nthChild(1,node);
+            struct nodeType *parent=NULL;
+            struct SymTableEntry* arrayEnt=NULL;
+            
+            // Find arrayID
+            while(arrtype->nodeType == NODE_ARR_REF){
+              arrtype = arrtype->child;
+            } 
+            // Get array node. update "node->valueType".
+            arrayEnt = findSymbol(arrtype->string);
+            arrnode = arrayEnt->link;
+            node->valueType = arrayEnt->type;
+            
+            // check the indexes from bottom(arrtype) to up(node).
+            parent = arrtype->parent->parent;
+            child2 = arrtype->rsibling;
+//                semanticCheck(child2);
+            do{
+              
+              if( child2->nodeType != NODE_INT)
+                // check the rsb and update it's valueType and iValue.
+              
+              if(child2->valueType!=TypeInt){
+                printf("index not Integer\n");
+                exit(0);
+              }
+
+              else if( (child2->iValue > arrnode->idxend) ||
+                       (child2->iValue < arrnode->idxstart) ){
+                printf("array %s index %d out of boundery %d..%d .\n",
+                       arrtype->string, child2->iValue, arrnode->idxstart,
+                       arrnode->idxend);                      
+              //  exit(0);
+              }
+
+              arrnode = arrnode->child;
+              parent = parent->parent;
+              child2 = parent->rsibling;
+ //               semanticCheck(child2);
+
+              if(arrnode ==NULL) {
+                printf("too much references.\n");
+                exit(0);
+              }
+
+
+            }while (parent != node);
+            
+
+            return;
+        }
         case NODE_TYPE_ARRAY: {
+            // won't happen.
             node->valueType = TypeArray;
+            printf("yo\n");
             return;
         }
         case NODE_INT: {
@@ -123,8 +197,58 @@ void semanticCheck(struct nodeType *node) {
         }
 
         /* Only implemented binary op here, you should implement unary op */
-        case NODE_OP:
+        case NODE_OP:{
+            //TODO check the both side have the same type.
+            struct nodeType *child1 = nthChild(1,node);
+            struct nodeType *child2 = nthChild(2,node);
+            semanticCheck(child1);
+            semanticCheck(child2);
+            if(child1->valueType == child2->valueType){
+            node->valueType = child1->valueType;
 
+            switch( node->op){
+              case OP_ADD:
+                if(node->valueType == TypeInt)
+                  node->iValue = child1->iValue + child2->iValue;
+                else 
+                  node->rValue = (float)child1->rValue + (float)child2->rValue;
+                break;
+              case OP_SUB:
+                if(node->valueType == TypeInt)
+                  node->iValue = child1->iValue - child2->iValue;
+                else 
+                  node->rValue = (float)child1->rValue - (float)child2->rValue;
+                break;
+              case OP_MUL:
+                if(node->valueType == TypeInt)
+                  node->iValue = child1->iValue * child2->iValue;
+                else 
+                  node->rValue = (float)child1->rValue * (float)child2->rValue;
+                break;
+              case OP_DIV:
+                if(node->valueType == TypeInt)
+                  node->iValue = child1->iValue / child2->iValue;
+                else 
+                  node->rValue = (float)child1->rValue / (float)child2->rValue;
+                break;
+              case OP_GT :
+                break;
+              case OP_LT :
+                break;
+              case OP_EQ :
+                break;
+              case OP_GE :
+                break;
+              case OP_LE :
+                break;
+              case OP_NE :
+                break;
+              case OP_NOT:
+                break;
+            }
+            return; 
+            }
+            }
         /* You should check the LHS of assign stmt is assignable
            You should also report error if LHS is a function with no parameter 
            (function is not implemented in this sample, you should implement it) */ 
@@ -139,8 +263,10 @@ void semanticCheck(struct nodeType *node) {
             if(child1->valueType != child2->valueType) {
                 if(node->nodeType == NODE_OP)
                     printf("Error: type mismatch for operator\n");
-                else
+                else{
                     printf("Error: type mismatch for assignment\n");
+                    printf("type: %d, %d\n",child1->valueType,child2->valueType);
+                    }
                 exit(0);
             }
 
